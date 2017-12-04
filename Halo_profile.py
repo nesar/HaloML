8502f60b5c028935af656ca5d95d71e2adcd6c72
WangYuyu <Yuyu@WangYuyus-MacBook-Pro.local>
2017-12-04 15:05:55 -0600

    add data

--- Halo_profile.py
+++ Halo_profile.py
@@ -15,11 +15,17 @@ min_axis_lim = -50.
 shift_position_constant = 100.
 
 bins=0.2   
-tbins=5.
-n_bin = 40
+tbins=1.5
+n_bin = 30
 
 intervals = np.logspace(np.log10(bins), np.log10(tbins), num = n_bin)
 radius = intervals[1:]
+
+d_critical = 1.3598e11
+m_particle = 3.65235543e10
+
+tolerance =1.e11
+tolerance_r = 1.e-5
 
 #intervals = np.array([nn*bins for nn in range(1,int(tbins/bins)+1)])
 #radius = intervals[1:]
@@ -114,42 +120,59 @@ def distance_calc(gals_pos,sph_pos, axis):
 def dist_mag(dx,dy,dz):
     dist_m = np.sqrt(dx**2+dy**2+dz**2)
     return dist_m
-      
-def halo_profile(pos, intervals, radius):
-    random_pos = pos[0]
     
-    dxx = distance_calc(pos,random_pos,0)[0] + random_pos[0]
-    dyy = distance_calc(pos,random_pos,1)[0] + random_pos[1]
-    dzz = distance_calc(pos,random_pos,2)[0] + random_pos[2]
+def r_200_cal(distance):
+    r_0 = 0.
+    r_1 = 2.
+    r_2 = 4.
+    while True:
+        index = np.array([i for i, j in enumerate(distance) if j <= r_1])
+        #print index
+        #print r_0, r_1, r_2
+        mass = m_particle*len(index)
+        den = 3.*mass/(4.*np.pi*(r_1**3))
+        #print den
+        if np.abs(den-200.*d_critical)<=tolerance:
+            break
+        if (r_1-r_0)<=tolerance_r:
+            r_1 = r_0
+            break
+        if (den-200.*d_critical)>tolerance:
+            r_0 = r_1
+            r_1 = (r_1+r_2)/2.
+        if (den-200.*d_critical)<-tolerance:
+            r_2 = r_1
+            r_1 = (r_0+r_1)/2.        
+    return r_1, mass
+        
+def halo_profile(pos, cor_m, intervals, radius):
     
-    cx = np.average(dxx)
-    cy = np.average(dyy)
-    cz = np.average(dzz)
-    
-    cor_m = np.array([cx,cy,cz])
-    new_pos = np.array([list(t) for t in zip(dxx,dyy,dzz)])
-    
-    dx = distance_calc(new_pos,cor_m,0)[0]
-    dy = distance_calc(new_pos,cor_m,1)[0]
-    dz = distance_calc(new_pos,cor_m,2)[0]
+    dx = distance_calc(pos,cor_m,0)[0]
+    dy = distance_calc(pos,cor_m,1)[0]
+    dz = distance_calc(pos,cor_m,2)[0]
     dist_temp = dist_mag(dx,dy,dz)
+    distance = dist_temp
     
-    mass_profile = np.histogram(dist_temp,bins = intervals)[0]
-    density_profile = mass_profile/(radius**3-intervals[:len(intervals)-1]**3)
+    info = r_200_cal(distance)
+    r_200 = info[0]
+    m_halo = info[1]
     
-    return mass_profile, density_profile
+    mass_profile = m_particle*np.histogram(dist_temp,bins = intervals)[0]
+    density_profile = 3.*mass_profile/(4.*np.pi*(radius**3-intervals[:len(intervals)-1]**3))
+    
+    return mass_profile, density_profile, r_200, m_halo
     
 
-fName = 'GADGET/groups_catalogue/fof_special_catalogue_064'
+fName = '/Users/Yuyu/Halo/GADGET/groups_catalogue/fof_special_catalogue_064'
 
 
 aGroupLen, aGroupOffset, aGroupMass, aGroupCM, aGroupNspecies, aGroupSfr = read_groups_catalogue(fName)
 
-fName1 = 'GADGET/groups_particles/fof_special_particles_064'
+fName1 = '/Users/Yuyu/Halo/GADGET/groups_particles/fof_special_particles_064'
 
 aPos, aNtot = read_groups_particles(fName1)
 
-fName2 = 'GADGET/groups_indexlist/fof_special_indexlist_064'
+fName2 = '/Users/Yuyu/Halo/GADGET/groups_indexlist/fof_special_indexlist_064'
 
 ntot, idx = read_groups_indexlist(fName2)
 
@@ -184,15 +207,22 @@ particle_start = particle_start[index_100]
 
 m_profile = np.zeros((len(group_id),len(radius)))
 d_profile = np.zeros((len(group_id),len(radius)))
+r_200 = np.zeros(len(group_id))
+m_halo = np.zeros(len(group_id))
 
 for i in range(len(group_id)):
     print i
     pos = cor[particle_start[i]:particle_start[i]+group_size[i]]
-    profile = halo_profile(pos, intervals, radius)
+    cor_m = aGroupCM[i]/1000.
+    profile = halo_profile(pos, cor_m, intervals, radius)
     
     m_profile[i] = profile[0]
     d_profile[i] = profile[1]
+    r_200[i] = profile[2]
+    m_halo[i] = profile[3]
+    #print r_200[i], m_halo[i]
     
-np.save('GADGET/fof-064-m_profile', m_profile)
-np.save('GADGET/fof-064-d_profile', d_profile)
-  
+np.save('/Users/Yuyu/Halo/GADGET/fof-064-m_profile', m_profile)
+np.save('/Users/Yuyu/Halo/GADGET/fof-064-d_profile', d_profile)
+np.save('/Users/Yuyu/Halo/GADGET/fof-064-r_200', r_200)
+np.save('/Users/Yuyu/Halo/GADGET/fof-064-m_200', m_halo)
--- Halo_profile_plot.py
+++ Halo_profile_plot.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 bins=0.2
-tbins=5.
-n_bin = 40
+tbins=1.5
+n_bin = 30
 
 intervals = np.logspace(np.log10(bins), np.log10(tbins), num = n_bin)
 radius = intervals[1:]
@@ -12,8 +12,8 @@ radius = intervals[1:]
 #radius = intervals[1:]
 
 
-m_profile = np.load('GADGET/fof-064-m_profile.npy')
-d_profile = np.load('GADGET/fof-064-d_profile.npy')
+m_profile = np.load('/Users/Yuyu/Halo/GADGET/fof-064-m_profile.npy')
+d_profile = np.load('/Users/Yuyu/Halo/GADGET/fof-064-d_profile.npy')
 
 m_aver = np.average(m_profile, axis=0)
 d_aver = np.average(d_profile, axis=0)
@@ -31,67 +31,14 @@ d_std = np.std(d_profile, axis=0)
 #plt.yscale("log", nonposy='clip')
 #plt.show()
 
-cut = 0
+d_critical = 1.3598e11
 
-plt.figure(100)
-for h_num in range(50,150, 1):
+plt.figure()
+for h_num in range(900,1100):
     #plt.errorbar(radius, d_aver, yerr=d_std, color='r')
-    plt.plot(radius[d_profile[h_num] > cut], d_profile[h_num][d_profile[h_num] > cut], 'ro-', alpha = 0.1, markersize = 3)
-    #plt.scatter(radius, d_profile[h_num], color='r')
-plt.xscale("log", nonposx='clip')
-plt.yscale("log", nonposy='clip')
-
-
-for h_num in range(350,450,1):
-    #plt.errorbar(radius, d_aver, yerr=d_std, color='r')
-    plt.plot(radius[d_profile[h_num] > cut], d_profile[h_num][d_profile[h_num] > cut], 'bo:', alpha = 0.1, markersize = 3)
-    #plt.scatter(radius, d_profile[h_num], color='r')
-plt.xscale("log", nonposx='clip')
-plt.yscale("log", nonposy='clip')
-
-for h_num in range(900,1000,1):
-    #plt.errorbar(radius, d_aver, yerr=d_std, color='r')
-    plt.plot(radius[d_profile[h_num] > cut], d_profile[h_num][d_profile[h_num] > cut], 'go--', alpha = 0.1, markersize = 3)
+    dd = d_profile[h_num]
+    plt.plot(radius[dd>0], dd[dd>0]/d_critical)
     #plt.scatter(radius, d_profile[h_num], color='r')
 plt.xscale("log", nonposx='clip')
 plt.yscale("log", nonposy='clip')
 plt.show()
-
-print('d_profile[50]')
-
-
-## --------------- i/o export --------------
-d_profile_list = []
-for h_num in range(0,1000,1):
-    d_profile_list.append(d_profile[h_num][d_profile[h_num] > cut].tolist())
-
-print(d_profile_list)
-# array.insert(0,var)
-
-import json
-with open('density_profile.txt', 'w') as outfile:
-    json.dump(d_profile_list, outfile)
-
-
-## --------------- i/o export parameter -- just mass right now --------------
-d_profile_list = []
-for h_num in range(0,1000,1):
-    d_profile_list.append(d_profile[h_num][d_profile[h_num] > cut].tolist())
-
-print(d_profile_list)
-# array.insert(0,var)
-
-import json
-with open('density_profile.txt', 'w') as outfile:
-    json.dump(d_profile_list, outfile)
-
-
-# halo_para = (1e10*np.abs(np.random.standard_normal(1000))).tolist()
-halo_para = np.random.randint(10, size = 1000).tolist()
-with open('halo_parameters.txt', 'w') as outfile:
-    json.dump(halo_para, outfile)
-
-
-
-
-
