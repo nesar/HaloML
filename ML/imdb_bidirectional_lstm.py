994f318841b7f3ed386a8e88666c0fd87450f3f3
WangYuyu <Yuyu@WangYuyus-MacBook-Pro.local>
2018-03-26 16:41:43 -0500

    new model

--- ML/imdb_bidirectional_lstm.py
+++ ML/imdb_bidirectional_lstm.py
@@ -13,11 +13,12 @@ from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.datasets import imdb
 
 
-max_features = 10000
+max_features = 1000
 # cut texts after this number of words
 # (among top max_features most common words)
 maxlen = 100
 batch_size = 32
+epochs=10
 
 print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
@@ -46,3 +47,113 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=4,
           validation_data=[x_test, y_test])
+          
+y_pred = model.predict(x_test)
+
+plotLossAcc = False
+if plotLossAcc:
+    import matplotlib.pylab as plt
+
+    train_loss= model.fit.history['loss']
+    val_loss= model.fit.history['val_loss']
+    train_acc= model.fit.history['acc']
+    val_acc= model.fit.history['val_acc']
+    epoch_array = range(1, epochs+1)
+
+
+    fig, ax = plt.subplots(2,1, sharex= True, figsize = (7,5))
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace= 0.02)
+    ax[0].plot(epoch_array,train_loss)
+    ax[0].plot(epoch_array,val_loss)
+    ax[0].set_ylabel('loss')
+    # ax[0].set_ylim([0,1])
+    # ax[0].set_title('Loss')
+    ax[0].legend(['train_loss','val_loss'])
+
+
+    ax[1].plot(epoch_array,train_acc)
+    ax[1].plot(epoch_array,val_acc)
+    ax[1].set_ylabel('acc')
+    # ax[0].set_ylim([0,1])
+    # ax[0].set_title('Loss')
+    ax[1].legend(['train_acc','val_acc'])
+
+    plt.show()
+
+
+plotLoss = True
+if plotLoss:
+    import matplotlib.pylab as plt
+
+    train_loss= model.fit.history['loss']
+    val_loss= model.fit.history['val_loss']
+    epoch_array = range(1, epochs+1)
+
+
+    fig, ax = plt.subplots(1,1, sharex= True, figsize = (7,5))
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace= 0.02)
+    ax.plot(epoch_array,train_loss)
+    ax.plot(epoch_array,val_loss)
+    ax.set_ylabel('loss')
+    # ax[0].set_ylim([0,1])
+    # ax[0].set_title('Loss')
+    ax.legend(['train_loss','val_loss'])
+
+    plt.show()
+
+#----------- Plot prediction vs truth ----------- 
+ScatterPredReal = True
+if ScatterPredReal:
+    
+    diff = np.abs(y_pred.T - y_test)
+
+    print('Difference min, max, mean, std, median')
+    print(np.min(diff), np.max(diff), np.mean(diff), np.std(diff), np.median(diff))
+
+    fig, ax = plt.subplots(2,1, figsize = (4,10))
+    fig.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace= 0.3)
+    ax[0].scatter(y_pred.T, y_test)
+    ax[0].set_ylabel('y_test[0] --- m200')
+    ax[0].set_xlabel('y_pred[0]')
+
+
+    plt.show()
+
+
+ScatterPred= True
+if ScatterPred:
+    fig, ax = plt.subplots(2,1, figsize = (4,10))
+    fig.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace= 0.3)
+    ax[0].scatter(y_test[:,0], y_pred[:,0]/y_test[:,0])
+    ax[0].set_ylabel('pred/test --- m200')
+    ax[0].set_xlabel('y_test[0]')
+    #ax[0].set_ylim(0,2)
+
+    ax[1].scatter(y_test[:,0], y_pred[:,1]/y_test[:,1])
+    ax[1].set_ylabel('pred/test --- r200')
+    ax[1].set_xlabel('y_test[1] --- m200')
+    ax[0].set_title('pred/test ratio variation with mass and radius')
+
+    plt.show()
+
+
+JointDistribution = True
+if JointDistribution:
+    fig, ax = plt.subplots(1,1, figsize = (5,5))
+    fig.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace= 0.3)
+    ax.scatter(y_test[:,0], y_test[:,1], label = 'y_test')
+    ax.set_ylabel('m200')
+    ax.set_xlabel('r200')
+    # ax.set_title()
+
+    ax.scatter(y_pred[:,0], y_pred[:,1], label = 'y_pred')
+    ax.set_ylabel('m200')
+    ax.set_xlabel('r200')
+    ax.set_title('Joint distribution')
+    plt.legend()
+
+    plt.show()
+
+
+ 
+
---
+++ halo_ML_test.py
@@ -0,0 +1,246 @@
+'''
+Trains and evaluate a simple MLP
+based on the Reuters newswire topic classification task.
+'''
+from __future__ import print_function
+
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional, Activation
+from keras.optimizers import Adam
+
+#import json
+m_particle = 1.35e8
+max_words = 30 # 1000
+
+batch_size = 32
+epochs = 10
+l_r = 0.002
+d_r = 0.001
+
+print('Loading data...')
+
+# x_train = density profile.
+
+class density_profile:
+    def __init__(self, data_path, para_path1, para_path2, test_split = 0.2, num_para = 1):
+        self.data_path = data_path
+        self.para_path1 = para_path1
+        self.para_path2 = para_path2
+        self.num_para = num_para
+        self.test_split = test_split
+
+
+    def open_data(self):                                                        
+#        with open(self.data_path) as json_data:
+#            self.allData = json.load(json_data)
+#
+#        with open(self.para_path) as json_data:
+#            self.allPara = json.load(json_data)
+        
+        self.allData = np.load(self.data_path)
+        self.allPara1 = np.load(self.para_path1)
+        self.allPara2 = np.load(self.para_path2)
+        
+        return self.allData, self.allPara1, self.allPara2
+
+    def load_data(self): # randomize and split into train and test data
+
+        allData, allPara1, allPara2 = self.open_data()
+        num_files = len(allData)   
+        print (num_files)                                             
+        num_train = int((1-self.test_split)*num_files)
+
+        np.random.seed(1234)
+        shuffleOrder = np.arange(num_files)
+        np.random.shuffle(shuffleOrder)
+        allData = allData[shuffleOrder]/(m_particle/1e4)
+        allPara1 = allPara1[shuffleOrder]/(1e6*m_particle)
+        allPara2 = allPara2[shuffleOrder]/1000.
+        allPara = np.dstack((allPara1, allPara2))[0]
+        print (allPara.shape)
+
+        self.x_train = allData[0:num_train]
+        self.y_train = allPara[0:num_train]
+
+        self.x_test = allData[num_train:num_files]
+        self.y_test = allPara[num_train:num_files]
+
+        return (self.x_train, self.y_train), (self.x_test, self.y_test)
+
+
+
+
+density_file = 'Bolshoi_All_density_profile.npy'                                            
+halo_para_file1 = 'Bolshoi_mass.npy'
+halo_para_file2 = 'Bolshoi_Radius.npy'
+#density_file = 'fof-064-d_profile.npy'                                            
+#halo_para_file1 = 'fof-064-m_200.npy'
+#halo_para_file2 = 'fof-064-r_200.npy'
+dens = density_profile(data_path = density_file, para_path1 = halo_para_file1, para_path2 = halo_para_file2)
+
+(x_train, y_train), (x_test, y_test) = dens.load_data()
+
+
+
+print(x_train.shape, 'train sequences')
+print(x_test.shape, 'test sequences')
+print(y_train.shape, 'train sequences')
+print(y_test.shape, 'test sequences')
+
+
+
+# ------------------------------------------
+
+max_features = int(np.max(x_train))+1
+
+print('Building model...')
+model = Sequential()
+model.add(Embedding(max_features, max_words*2, input_length=max_words))
+model.add(Bidirectional(LSTM(max_words*2)))
+model.add(Dropout(0.5))
+model.add(Dense(20))
+model.add(Activation('relu'))
+model.add(Dense(15))
+model.add(Activation('relu'))
+model.add(Dense(10))
+model.add(Activation('relu'))
+model.add(Dense(5))
+model.add(Activation('relu'))
+model.add(Dense(2))
+model.add(Activation('linear'))
+
+adam = Adam(lr = l_r, decay = d_r)
+
+#model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer=adam, metrics=['accuracy'])
+#model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+
+ModelFit = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+
+
+print('---------------------------')
+
+#--------------Testing----------------------------
+
+y_pred = model.predict(x_test)
+
+#-------------------------------------------------
+
+plotLossAcc = False
+if plotLossAcc:
+    import matplotlib.pylab as plt
+
+    train_loss= ModelFit.history['loss']
+    val_loss= ModelFit.history['val_loss']
+    train_acc= ModelFit.history['acc']
+    val_acc= ModelFit.history['val_acc']
+    epoch_array = range(1, epochs+1)
+
+
+    fig, ax = plt.subplots(2,1, sharex= True, figsize = (7,5))
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace= 0.02)
+    ax[0].plot(epoch_array,train_loss)
+    ax[0].plot(epoch_array,val_loss)
+    ax[0].set_ylabel('loss')
+    # ax[0].set_ylim([0,1])
+    # ax[0].set_title('Loss')
+    ax[0].legend(['train_loss','val_loss'])
+
+
+    ax[1].plot(epoch_array,train_acc)
+    ax[1].plot(epoch_array,val_acc)
+    ax[1].set_ylabel('acc')
+    # ax[0].set_ylim([0,1])
+    # ax[0].set_title('Loss')
+    ax[1].legend(['train_acc','val_acc'])
+
+    plt.show()
+
+
+plotLoss = True
+if plotLoss:
+    import matplotlib.pylab as plt
+
+    train_loss= ModelFit.history['loss']
+    val_loss= ModelFit.history['val_loss']
+    epoch_array = range(1, epochs+1)
+
+
+    fig, ax = plt.subplots(1,1, sharex= True, figsize = (7,5))
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace= 0.02)
+    ax.plot(epoch_array,train_loss)
+    ax.plot(epoch_array,val_loss)
+    ax.set_ylabel('loss')
+    # ax[0].set_ylim([0,1])
+    # ax[0].set_title('Loss')
+    ax.legend(['train_loss','val_loss'])
+
+    plt.show()
+
+#----------- Plot prediction vs truth ----------- 
+ScatterPredReal = True
+if ScatterPredReal:
+    
+    diff = np.abs(y_pred[:,0] - y_test[:,0])
+
+    print('Difference min, max, mean, std, median')
+    print(np.min(diff), np.max(diff), np.mean(diff), np.std(diff), np.median(diff))
+
+    fig, ax = plt.subplots(2,1, figsize = (4,10))
+    fig.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace= 0.3)
+    ax[0].scatter(y_pred[:,0], y_test[:,0])
+    ax[0].set_ylabel('y_test[0] --- m200')
+    ax[0].set_xlabel('y_pred[0]')
+
+    ax[1].scatter(y_pred[:,1], y_test[:,1])
+    ax[1].set_ylabel('y_test[1] --- r200')
+    ax[1].set_xlabel('y_pred[1]')
+
+    plt.show()
+
+
+ScatterPred= True
+if ScatterPred:
+    fig, ax = plt.subplots(2,1, figsize = (4,10))
+    fig.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace= 0.3)
+    ax[0].scatter(y_test[:,0], y_pred[:,0]/y_test[:,0])
+    ax[0].set_ylabel('pred/test --- m200')
+    ax[0].set_xlabel('y_test[0]')
+    #ax[0].set_ylim(0,2)
+
+    ax[1].scatter(y_test[:,0], y_pred[:,1]/y_test[:,1])
+    ax[1].set_ylabel('pred/test --- r200')
+    ax[1].set_xlabel('y_test[1] --- m200')
+    ax[0].set_title('pred/test ratio variation with mass and radius')
+
+    plt.show()
+
+
+JointDistribution = True
+if JointDistribution:
+    fig, ax = plt.subplots(1,1, figsize = (5,5))
+    fig.subplots_adjust(left=0.2, bottom=None, right=None, top=None, wspace=None, hspace= 0.3)
+    ax.scatter(y_test[:,0], y_test[:,1], label = 'y_test')
+    ax.set_ylabel('m200')
+    ax.set_xlabel('r200')
+    # ax.set_title()
+
+    ax.scatter(y_pred[:,0], y_pred[:,1], label = 'y_pred')
+    ax.set_ylabel('m200')
+    ax.set_xlabel('r200')
+    ax.set_title('Joint distribution')
+    plt.legend()
+
+    plt.show()
+
+
+ 
